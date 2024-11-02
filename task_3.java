bef458cc4e50702f58021ff3e9780a13df82ca8c
Marina Kuzmenko <mary.kuzmenko2016@yandex.kz>
2024-11-02 12:44:49 +0500

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+class PalindromeService {
+
+    public boolean isPalindrome(String text) {
+        String reversedText = new StringBuilder(text).reverse().toString();
+        return text.equals(reversedText);
+    }
+}
+
+@RunWith(Parameterized.class) // добавь аннотацию с раннером
+public class PalindromeServiceTest {
+
+	private final String checkedText;
+    private final boolean expected;// добавь два поля класса: для проверяемой строки и ожидаемого результата
+
+		public PalindromeServiceTest(String checkedText, boolean expected) {
+            this.checkedText = checkedText;
+            this.expected = expected;
+       
+        } 
+    @Parameterized.Parameters
+        public static Object[][] getTestData() {
+        return new Object[][] {
+                {"anna", true},
+                {"anka", false},
+                {"q", true},
+        };
+    }
+		
+    @Test
+    public void shouldCheckPalindrome() {
+     PalindromeService palindromeService = new PalindromeService();
+        boolean actual = palindromeService.isPalindrome(checkedText);
+        assertEquals(expected, actual);
+    }
+		// напиши тест
+
+}
